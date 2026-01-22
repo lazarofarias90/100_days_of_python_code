bd8cdaeb4805904a4cc4d254ccae430604b1106b
lazarofarias90 <rochalazaro90@gmail.com>
2026-01-22 10:38:09 -0300

    Finding a way to identify the different types of input

---
+++ Day_02/task_02.py
@@ -0,0 +1,4 @@
+print(type("abc")) # will give you <class 'str'>
+print(type(123)) # will give you <class 'int'>
+print(type(3.1415)) # will give you <class 'float'>
+print(type(True)) # will give you <class 'bool'>
