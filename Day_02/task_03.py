00b0d91837b6bb451f342a5c94f5c3f7bca3c982
lazarofarias90 <rochalazaro90@gmail.com>
2026-01-22 13:41:30 -0300

    Learning how to use basic mathematical operators in Python.

---
+++ Day_02/task_03.py
@@ -0,0 +1,10 @@
+print(123 + 456) # sum operation
+print(7 - 3) # subtraction operation
+print(3 * 2) # multiplication operation
+print(5 / 3) # division operation
+print(5 // 3) # division operation without remainder
+print(2 ** 3) # exponentiation operation
+
+# PEMDAS - Parentheses, Exponents, Multiplication/Division, Addition/Subtraction
+
+print(3 * 3 + 3 / 3 - 3) 
