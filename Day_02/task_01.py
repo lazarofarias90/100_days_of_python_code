71ecbf791d08cdcfd83835f554adb4e0da3ec88a
lazarofarias90 <rochalazaro90@gmail.com>
2026-01-22 09:15:00 -0300

    Learning about primitive data types in Python.

---
+++ Day_02/task_01.py
@@ -0,0 +1,18 @@
+# Subscripiting
+print("Hello"[0])
+
+# String
+print("123" + "345")
+
+# Integer = Whole number
+print(123 + 345)
+
+# Large Integers
+print(123_456_789)
+
+# Float = Float point number
+print(3.14159)
+
+# Boolean
+print(True)
+print(False)
