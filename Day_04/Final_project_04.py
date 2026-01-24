76d1e0eb1860346df07c63b8cfafa04d7a1ee656
lazarofarias90 <rochalazaro90@gmail.com>
2026-01-24 17:02:57 -0300

    Finishing up the final project for the 4th.

---
+++ Day_04/Final_project_04.py
@@ -0,0 +1,141 @@
+import random
+
+print('Welcome to the "Rock, Paper, Scissors" game.')
+print("What do you choose?")
+player_choice = int(input("Type 0 for Rock, 1 for Paper o 2 for Scissors: "))
+computer_choice = random.randint(0, 2)
+
+if player_choice == 0:
+    print("Your choose:")
+    print("""
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+""")
+    if computer_choice == 0:
+        print("COmputer choose:")
+        print("""
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+""")
+        print("Nobody won!")
+    elif computer_choice == 1:
+        print("COmputer choose:")
+        print("""
+     _______
+---'    ____)____
+           ______)
+          _______)
+         _______)
+---.__________)
+""")
+        print("You lose!")
+    else:
+        print("COmputer choose:")
+        print("""
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+""")
+        print("You win!")
+
+if player_choice == 1:
+    print("Your choose:")
+    print("""
+     _______
+---'    ____)____
+           ______)
+          _______)
+         _______)
+---.__________)
+""")
+    if computer_choice == 0:
+        print("COmputer choose:")
+        print("""
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+""")
+        print("You win!")
+    elif computer_choice == 1:
+        print("COmputer choose:")
+        print("""
+     _______
+---'    ____)____
+           ______)
+          _______)
+         _______)
+---.__________)
+""")
+        print("Nobody won!")
+    else:
+        print("COmputer choose:")
+        print("""
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+""")
+        print("You lose!")
+
+if player_choice == 2:
+    print("Your choose:")
+    print("""
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+""")
+    if computer_choice == 0:
+        print("COmputer choose:")
+        print("""
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+""")
+        print("You lose!")
+    elif computer_choice == 1:
+        print("COmputer choose:")
+        print("""
+     _______
+---'    ____)____
+           ______)
+          _______)
+         _______)
+---.__________)
+""")
+        print("You win!")
+    else:
+        print("COmputer choose:")
+        print("""
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+""")
+        print("Nobody won!")
+
+else:
+    print("You choose the wrong number!")
