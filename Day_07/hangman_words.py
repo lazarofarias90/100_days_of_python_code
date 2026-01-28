05912a13b2d0f567054d873391757916246c2d2f
lazarofarias90 <rochalazaro90@gmail.com>
2026-01-28 19:33:08 -0300

    Adding hangman words

---
+++ Day_07/hangman_words.py
@@ -0,0 +1,13 @@
+word_list = [
+
+    'abelha', 'arvore', 'bananas', 'caverna', 'dragao', 'escada', 'foguete', 
+
+    'girafa', 'harpa', 'ilha', 'janela', 'kiwi', 'limao', 'montanha', 
+
+    'navio', 'oculos', 'palhaço', 'queijo', 'rato', 'sapato', 'tigre', 
+
+    'urso', 'vassoura', 'xicara', 'zebra', 'computador', 'python', 'linux',
+
+    'bootcamp', 'teclado', 'monitor', 'algoritmo', 'biblioteca', 'codigo'
+
+]
