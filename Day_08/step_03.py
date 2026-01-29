3cf91198ac9ea41a5dab6ba1ec45f75608114be0
lazarofarias90 <rochalazaro90@gmail.com>
2026-01-29 11:28:35 -0300

    Positional vs Keyword Arguments

---
+++ Day_08/step_03.py
@@ -0,0 +1,7 @@
+def greet_with(name, location):
+    print(f"Hello, {name}!")
+    print(f"What is it like in {location}")
+
+greet_with("Angela", "England")
+
+# greet_with(name="Angela", location="Engrland")
