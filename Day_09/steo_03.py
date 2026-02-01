9bb1b888d9d1eaa63bbf6e53d41db4f7d65651a2
lazarofarias90 <rochalazaro90@gmail.com>
2026-01-31 21:52:23 -0300

    feat: :sparkles: Nesting Lists and Dictionaries

---
+++ Day_09/steo_03.py
@@ -0,0 +1,29 @@
+capitals = {
+    "France": "Paris",
+    "Germany": "Berlin",
+}
+
+# Nested List in Dictionary
+# travel_log = {
+#     "France": ["Paris", "Lille", "Dijon"],
+#     "Germany": ["Stuttgart", "Berlin"],
+# }
+
+# Print Lille
+# print(travel_log["France"][1])
+
+nested_list = ["A", "B", ["C", "D"]]
+# print(nested_list[2][1])
+
+travel_log = {
+    "France": {
+        "num_times_visited": 8,
+        "cities_visited": ["Paris", "Lille", "Dijon"]
+    },
+    "Germany": {
+        "num_times_visited": 5,
+        "cities_visited": ["Berlin", "Hamburg", "Stuttgart"]
+    },
+}
+
+print(travel_log["Germany"]["cities_visited"][2])
