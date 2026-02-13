841f68af5dcd8a25cbe698464f9fbeacb3087387
lazarofarias90 <rochalazaro90@gmail.com>
2026-02-13 16:39:08 -0300

    feat: :sparkles: bringing out the big gun

---
+++ Day_13/step_05.py
@@ -0,0 +1,14 @@
+# word_per_page = 0
+# pages = int(input("Number of pages: "))
+# word_per_page == int(input("Number of words per page: "))
+# total_words = pages * word_per_page
+# print(total_words)
+
+# In this case, to resolve the bug, we can use the print command to try and find the error. 
+# We can print the variables on the screen and thus be able to find the problem.
+
+word_per_page = 0
+pages = int(input("Number of pages: "))
+word_per_page = int(input("Number of words per page: "))
+total_words = pages * word_per_page
+print(total_words)
