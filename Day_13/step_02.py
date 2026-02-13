bcda3abeceeed6a8f99451e05593cabf989db7dc
lazarofarias90 <rochalazaro90@gmail.com>
2026-02-13 16:30:29 -0300

    feat: :sparkles: play computer and evaluate each line

---
+++ Day_13/step_02.py
@@ -0,0 +1,12 @@
+# from random import randint
+# dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
+# dice_num = randint(1, 6)
+# print(dice_imgs[dice_num])
+
+# Reproducing the error in its entirety.
+
+from random import randint
+dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
+# dice_num = randint(1, 6)
+# print(dice_imgs[dice_num])
+print(dice_imgs[6])
